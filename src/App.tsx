337932d5ca299d8dfef2afbbb0a0f0fa15140661
boardGMA <gwynn@gmboardadvisory.co.uk>
2025-06-26 15:23:05 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>GM Board Advisory</h1>
-      <p>Advisor | NED | Board Consultant</p>
-      <p>gwynn@gmboardadvisory.co.uk</p>
-      <p>+44(0)7720 089217</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
