10385b9d35724e80a9deacdaba7c35f83f939647
boardGMA <gwynn@gmboardadvisory.co.uk>
2025-06-26 13:07:04 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+unction Index() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>GM Board Advisory</h1>
+      <p>Advisor | NED | Board Consultant</p>
+      <p>gwynn@gmboardadvisory.co.uk</p>
+      <p>+44(0)7720 089217</p>
+    </div>
+  );
+}
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default Index;
